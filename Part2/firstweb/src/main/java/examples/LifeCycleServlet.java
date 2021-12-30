dac708a482a6cd735ec7c21ef06c2364334fdc1c
hyunmin <choihm9903@naver.com>
2021-12-30 16:03:51 +0900

    Update LifeCylce Servlet

---
+++ Part2/firstweb/src/main/java/examples/LifeCycleServlet.java
@@ -0,0 +1,72 @@
+package examples;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LifeCycleServlet
+ */
+@WebServlet("/LifeCycleServlet")
+public class LifeCycleServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LifeCycleServlet() {
+        super();
+        System.out.println("LifecycleServlet 생성!!");
+    }
+
+	/**
+	 * @see Servlet#init(ServletConfig)
+	 */
+	public void init(ServletConfig config) throws ServletException {
+		System.out.println("init test 호출!!");
+	}
+
+	/**
+	 * @see Servlet#destroy()
+	 */
+	public void destroy() {
+		System.out.println("destroy 호출!!");
+	}
+
+	/**
+	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
+	 */
+//	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//		System.out.println("service호출!!");		
+//	}
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		out.println("<html>");
+		out.println("<head><title>form</title></head>");
+		out.println("<body>");
+		out.println("<form method='post' action='/firstweb/LifeCycleServlet'>");
+		out.println("name : <input type='text' name='name'><br>");
+		out.println("<input type='submit' value='ok'><br>");                                                 
+		out.println("</form>");
+		out.println("</body>");
+		out.println("</html>");
+		out.close();
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		String name = request.getParameter("name");
+		out.println("<h1> hello " + name + "</h1>");
+		out.close();
+	}
+}
