85b1cbae12ba7680e4b0afdc9d88d76dd8c7de09
hyunmin <choihm9903@naver.com>
2021-12-31 14:33:57 +0900

    Update Test

--- Part3/calculatorcli/src/test/java/org/edwith/webbe/calculatorcli/CalculatorServiceTest2.java
+++ Part3/calculatorcli/src/test/java/org/edwith/webbe/calculatorcli/CalculatorServiceTest2.java
@@ -1,16 +1,19 @@
 package org.edwith.webbe.calculatorcli;
 
+
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-public class CalculatorServiceTest {
-    CalculatorService calculatorService;
-
-    @Before
-    public void init(){
-        this.calculatorService = new CalculatorService();
-    }
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {ApplicationConfig.class})
+public class CalculatorServiceTest2 {
+    @Autowired
+    CalculatorService2 calculatorService;
 
     @Test
     public void plus() throws Exception{
@@ -22,7 +25,7 @@ public class CalculatorServiceTest {
         int result = calculatorService.plus(value1, value2);
 
         // then
-        Assert.assertEquals(15, result); // 결과와 15가 같을 경우에만 성공
+        Assert.assertEquals(result, 15); // 결과와 15가 같을 경우에만 성공
     }
 
     @Test
@@ -32,10 +35,10 @@ public class CalculatorServiceTest {
         int value2 = 5;
 
         // when
-        int result = calculatorService. divide (value1, value2);
+        int result = calculatorService.divide(value1, value2);
 
         // then
-        Assert.assertEquals(2,result); // 결과와 2가 같을 경우에만 성공
+        Assert.assertEquals(result, 2); // 결과와 15가 같을 경우에만 성공
     }
 
     @Test
@@ -50,7 +53,7 @@ public class CalculatorServiceTest {
             Assert.assertTrue(true); // 이부분이 실행되었다면 성공
             return; // 메소드를 더이상 실행하지 않는다.
         }
+        
         Assert.assertTrue(false); // 이부분이 실행되면 무조건 실패다.
-
     }
 }
